4d944b2410e8ff08cf408857f1f8674b9ec49ee3
mariaeduardateixeira <mariaeduardat.silvano@gmail.com>
2024-08-17 13:29:43 -0300

    Regras da tela agenda-consulta

--- src/components/calendario/index.tsx
+++ src/components/calendario/index.tsx
@@ -2,17 +2,45 @@ import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { Dayjs } from 'dayjs'; // Importar Dayjs para o tipo de data
 import './styles.css';
 
 export default function BasicDateCalendar() {
+  const [selectedDate, setSelectedDate] = React.useState<Dayjs | null>(null);
+
+  // const handleDateChange = (date: Dayjs | null) => {
+  //   setSelectedDate(date);
+  //   console.log('Data selecionada:', date ? date.format('DD-MM-YYYY') : 'Nenhuma data selecionada');
+  // };
+
+  const handleDateChange = (date: Dayjs | null) => {
+    setSelectedDate(date);
+    if (date) {
+      const formattedDate = date.format('DD/MM/YYYY');
+      localStorage.setItem('selectedDate', formattedDate); // Armazena a data no localStorage
+      console.log('Data selecionada:', formattedDate);
+    } else {
+      localStorage.removeItem('selectedDate'); // Remove a data se não houver seleção
+    }
+  };
+  
+  const date = selectedDate ? selectedDate.format('DD/MM/YYYY') : 'Nenhuma data selecionada'
+  
   return (
     <div className='calendar'>
       <div className="titulo-calendario">
         <h1 className='titulo-calendar'>Escolha uma data e horário</h1>
       </div>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar className='calendar-container' />
+        <DateCalendar
+          className='calendar-container'
+          onChange={handleDateChange} // Adiciona o manipulador de mudança
+        />
       </LocalizationProvider>
+      {/* Exibe a data selecionada */}
+      <div className="selected-date">
+        Data Selecionada: {date}
+      </div>
     </div>
   );
 }
--- src/pages/agendarConsulta/index.tsx
+++ src/pages/agendarConsulta/index.tsx
@@ -1,10 +1,23 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Calendario from '../../components/calendario';
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import './styles.css';
+import { useNavigate } from 'react-router-dom';
 
 const AgendarConsulta: FC = () => {
-  const [medico, setMedico] = useState('');
+  const [tipoConsulta, setTipoConsulta] = useState('');
+  const [horarioConsulta, setHorarioConsulta] = useState('');
+  const [selectedDate, setSelectedDate] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handleContinuarClick = () => {
+      navigate('/paciente');
+  }
+
+  const handleVoltarClick = () => {
+    navigate('/home');
+  }
+
   return (
     <div className='container'>
       <div className="container-div">
@@ -13,26 +26,77 @@ const AgendarConsulta: FC = () => {
             <h1 className="titulo-consulta">Escolha o tipo de consulta</h1>
           </div>
           <div className="option-dropdown">
-            <TextField
-              className='medico'
-              margin="dense"
-              id="medico"
-              select
-              fullWidth
-              variant="standard"
-              value={medico}
-              onChange={(e) => setMedico(e.target.value)}
-              SelectProps={{
-                native: true,
-              }}
-            >
-              <option defaultValue="">Selecione uma opção</option>
-              <option value="dentista">Odontológica</option>
-              <option value="medico">Clínico Geral</option>
-            </TextField>
+          <TextField
+        className='tipo-consulta'
+        margin="dense"
+        id="tipo-consulta"
+        label="Selecione o tipo de consulta"
+        select
+        fullWidth
+        variant="standard"
+        value={tipoConsulta}
+        onChange={(e) => setTipoConsulta(e.target.value)}
+        SelectProps={{
+          native: true,
+        }}
+      >
+        <option value=""></option>
+        <option value="Odontológica">Odontológica</option>
+        <option value="Clínico Geral">Clínico Geral</option>
+      </TextField>
           </div>
+          <div className="container-resumo-consulta">
+          <h1 className='titulo-consulta'>Resumo da consulta</h1>
+          <p>Data:</p>
+          <p>Consulta: {tipoConsulta}</p>
+          <p>Horário: {horarioConsulta}</p>
+          <p>Local:</p>
         </div>
-        <Calendario />
+        </div>
+        <div className="dropdown-medico">
+          <Calendario />
+          <div className="option-dropdown">
+          <TextField
+        className='horario-consulta'
+        margin="dense"
+        id="horario-consulta"
+        label="Selecione o horário"
+        select
+        fullWidth
+        variant="standard"
+        value={horarioConsulta}
+        onChange={(e) => setHorarioConsulta(e.target.value)}
+        SelectProps={{
+          native: true,
+        }}
+      >
+        <option value=""></option>
+        <option value="09:00">09:00</option>
+        <option value="10:10">10:10</option>
+      </TextField>
+        </div>
+          <Button onClick={handleVoltarClick} variant="contained" color="primary">
+            Voltar
+          </Button>
+          <Button 
+            onClick={() => {
+              if(tipoConsulta === "" || horarioConsulta === ""){
+                //validar data selecionada também
+                alert("É necessário preencher todos os dados!")
+              }else if (horarioConsulta === "") {
+                window.alert("Por favor, selecione um horário!");
+              }else if (horarioConsulta === "") {
+                window.alert("Por favor, selecione um horário!");
+              } else{
+                handleContinuarClick();
+              }
+            }
+            } 
+            variant="contained" 
+            color="primary">
+            Continuar
+          </Button>
+        </div>
       </div>
     </div>
   );
