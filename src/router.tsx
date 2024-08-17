118b968ff617a4a857ef0161bf2178677a8d93dc
ribasmariana <mariana.248.ribas@gmail.com>
2024-08-17 17:22:13 -0300

    rota login

--- src/router.tsx
+++ src/router.tsx
@@ -14,12 +14,8 @@ const AppRouter = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/agendar-consulta" element={<AgendarConsulta />} />
       <Route path='/paciente' element={<Paciente/>}/>
-<<<<<<< HEAD
-      <Route path='/login' element={<Login/>}/>
-=======
       <Route path='/confirmacao-consulta' element={<EnvioEmail/>}/>
-      <Route path='/login-area-profissional' element={<Login/>}/>
->>>>>>> 7ecb50d198fa9cca36050c32413ee9750e8ec639
+      <Route path='/login' element={<Login/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
     </Routes>
   );
