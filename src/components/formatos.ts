6b34ed383952c870f4be4d34b8492a8b5dc51432
IsacGuedes <witcherthe0869@gmail.com>
2024-08-28 20:17:06 -0300

    Dashboard Todas Consultas
    Dashboard de todas as consultas atualizado, agora puxa todas as informações e também muda o status da consulta

--- src/components/formatos.ts
+++ src/components/formatos.ts
@@ -1,47 +1,49 @@
+// src/components/formatos.ts
+
 export const aplicarMascaraDocumento = (documento: string): string => {
+    // Função para aplicar máscara de CPF e CNS
     const apenasDigitos = documento.replace(/\D/g, '').slice(0, 15);
     
-    // Aplica máscara de CPF (11 dígitos)
     if (apenasDigitos.length <= 11) {
         return apenasDigitos
             .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-    } 
-    // Aplica máscara de CNS (15 dígitos)
-    else if (apenasDigitos.length <= 15) {
+    } else if (apenasDigitos.length <= 15) {
         return apenasDigitos
             .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
     } 
-    return documento; // Retorna o documento original se não atender aos casos acima
+    return documento;
 };
 
 export const formatarTelefone = (telefone: string, paraEnvio: boolean = false): string => {
     const apenasDigitos = telefone.replace(/\D/g, '').slice(0, 11);
 
     if (paraEnvio) {
-        // Adiciona o prefixo +55 para números brasileiros no envio
         return apenasDigitos.length === 10 || apenasDigitos.length === 11
             ? `+55${apenasDigitos}`
             : telefone;
     } else {
         if (apenasDigitos.length === 11) {
-            // Formata telefone celular (11 dígitos)
             return apenasDigitos
                 .replace(/(\d{2})(\d)/, '($1) $2')
                 .replace(/(\d{5})(\d{4})$/, '$1-$2');
         } else if (apenasDigitos.length === 10) {
-            // Formata telefone fixo (10 dígitos)
             return apenasDigitos
                 .replace(/(\d{2})(\d)/, '($1) $2')
                 .replace(/(\d{4})(\d{4})$/, '$1-$2');
         }
     }
 
-    return telefone; // Retorna o telefone original caso não se aplique nenhuma formatação
+    return telefone;
+};
+
+export const formatDate = (dateString: string): string => {
+    const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    return new Date(dateString).toLocaleDateString('pt-BR', options);
 };
 
 export const removerCaracteresNaoNumericos = (valor: string): string => {
