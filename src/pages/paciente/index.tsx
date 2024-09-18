f55310cc515b5b8bbaed3ea45de3800b14664e4e
ribasmariana <mariana.248.ribas@gmail.com>
2024-09-18 19:15:18 -0300

    validar cpg

--- src/pages/paciente/index.tsx
+++ src/pages/paciente/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react';
 import { TextField, Button, Typography, Radio, FormControlLabel, Grid } from '@mui/material';
 import { apiPost, STATUS_CODE } from '../../api/RestClient';
-import { aplicarMascaraDocumentocns, aplicarMascaraDocumentocpf, formatarTelefone, removerCaracteresNaoNumericos } from '../../components/formatos';
+import { aplicarMascaraDocumentocns, aplicarMascaraDocumentocpf, formatarTelefone, removerCaracteresNaoNumericos, validarCpf } from '../../components/formatos';
 import { useNavigate } from 'react-router-dom';
 import { InfoCircleOutlined } from '@ant-design/icons';
 import './styles.css';
@@ -20,7 +20,12 @@ const Paciente: FC = () => {
       alert("Preencha todos os campos obrigatórios.");
       return;
     }
-
+  
+    if (selectedDocType === 'cpf' && !validarCpf(documentocpf)) {
+      alert("CPF inválido. Por favor, verifique o número informado.");
+      return;
+    }
+  
     const data = {
       nome,
       documentocpf: removerCaracteresNaoNumericos(documentocpf),
@@ -28,19 +33,19 @@ const Paciente: FC = () => {
       telefone: formatarTelefone(removerCaracteresNaoNumericos(telefone), true),
       dataNascimento,
     };
-
+  
     try {
       const response = await apiPost("/pacientes/criarPaciente", data);
       if (response.status === STATUS_CODE.CREATED) {
         const pacienteId = response.data.id;
         const consultaData = JSON.parse(sessionStorage.getItem('consultaData') || '{}');
         consultaData.pacienteId = pacienteId;
-
+  
         if (!consultaData.dataConsulta || !consultaData.tipoConsulta || !consultaData.horario) {
           alert("Dados da consulta estão incompletos.");
           return;
         }
-
+  
         const consultaResponse = await apiPost("/agendar-consulta/criarConsulta", consultaData);
         if (consultaResponse.status === STATUS_CODE.CREATED) {
           alert("Paciente e consulta cadastrados com sucesso!");
@@ -55,6 +60,7 @@ const Paciente: FC = () => {
       alert("Erro ao conectar com o servidor.");
     }
   };
+  
 
   return (
     <div className="paciente">
