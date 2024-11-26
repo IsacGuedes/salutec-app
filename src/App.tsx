bf43983f37d0fdcb28ab0e7080075e08788f92ba
ribasmariana <mariana.248.ribas@gmail.com>
2024-11-25 21:28:54 -0300

    logout

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,31 @@ import './App.css';
 import AppRouter from './router';
 import Logo from './assets/logo.png';
 import { Link } from 'react-router-dom';
-import DashboardNavbar from './components/navbarDashboard';
-import Footer from './components/footer';
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const location = useLocation();
+  const navigate = useNavigate();
   const isDashboardRoute = location.pathname.startsWith('/dashboard');
+
+  // Lógica do logout
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Remove o token de autenticação
+    localStorage.removeItem('enfermeiroNome'); // Remove o nome do enfermeiro armazenado
+    navigate('/login'); // Redireciona para a página de login
+  };
 
   return (
     <div className="App">
       {isDashboardRoute ? (
         <>
-           <header className="menu-bar">
+          <header className="menu-bar">
             <div className="logo">
                 <img src={Logo} alt="Logo" className='logo-imagem' />
+            </div>
+            <div className='enfermeiro-info'>
+                <Button type="link" onClick={handleLogout}>Sair</Button> {/* Botão de logout no topo */}
             </div>
           </header>
           <AppRouter />
@@ -32,7 +43,6 @@ function App() {
             </div>
           </header>
           <AppRouter />
-          <Footer />
         </>
       )}
     </div>
